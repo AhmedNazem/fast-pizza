b37a2d29534b15f241fee282ef6440a1252fb3a0
AhmedNazem <aahhaammdd55@gmail.com>
2024-11-15 01:55:28 +0300

    build  cart functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -47,6 +47,8 @@ export const {
 } = cartSlice.actions;
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, cur) => sum + cur.quantity, 0);
 export const getTotalCartPrice = (state) =>
